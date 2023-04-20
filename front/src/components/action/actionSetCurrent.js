90782dc44e7142f4d7cdf143751fa833c804d4b8
zhennann <zhen.nann@icloud.com>
2023-04-20 08:37:17 +0800

    atom:action atomClass

--- front/src/components/action/actionSetCurrent.js
+++ front/src/components/action/actionSetCurrent.js
@@ -3,14 +3,18 @@ export default {
     async _onActionSetCurrent() {
       const { ctx, item } = this.$props;
       const key = { atomId: item.atomId, itemId: item.itemId };
+      const atomClass = {
+        module: item.module,
+        atomClassName: item.atomClassName,
+      };
       await ctx.$view.dialog.confirm();
       const result = await ctx.$api.post('/bz/login/backImage/setCurrent', { key });
       if (result) {
         // new
-        ctx.$meta.eventHub.$emit('atom:action', { key, action: { name: 'save' } });
+        ctx.$meta.eventHub.$emit('atom:action', { key, atomClass, action: { name: 'save' } });
         // old
         if (result.keyOld) {
-          ctx.$meta.eventHub.$emit('atom:action', { key: result.keyOld, action: { name: 'save' } });
+          ctx.$meta.eventHub.$emit('atom:action', { key: result.keyOld, atomClass, action: { name: 'save' } });
         }
       }
       return true;
