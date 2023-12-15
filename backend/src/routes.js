1a386f081cfb295aa6028f59adb1206bab7657df
zhennann <zhen.nann@icloud.com>
2023-12-15 20:22:21 +0800

    refactor: routes

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,18 +1,15 @@
-module.exports = app => {
-  const routes = [
-    // backImage
-    {
-      method: 'post',
-      path: 'backImage/setCurrent',
-      controller: 'backImage',
-      meta: { right: { type: 'atom', atomClass: 'bz-login:loginBackImage', action: 'setCurrent' } },
-    },
-    {
-      method: 'get',
-      path: 'backImage/current',
-      action: 'getCurrent',
-      controller: 'backImage',
-    },
-  ];
-  return routes;
-};
+module.exports = [
+  // backImage
+  {
+    method: 'post',
+    path: 'backImage/setCurrent',
+    controller: 'backImage',
+    meta: { right: { type: 'atom', atomClass: 'bz-login:loginBackImage', action: 'setCurrent' } },
+  },
+  {
+    method: 'get',
+    path: 'backImage/current',
+    action: 'getCurrent',
+    controller: 'backImage',
+  },
+];
