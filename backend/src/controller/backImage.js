9907ff5378808fe0ec58cb2876a96be0e66b8e59
zhennann <zhen.nann@icloud.com>
2023-12-15 22:24:07 +0800

    refactor: controllers

--- backend/src/controller/backImage.js
+++ backend/src/controller/backImage.js
@@ -1,19 +1,15 @@
-module.exports = app => {
-  class BackImageController extends app.Controller {
-    async setCurrent() {
-      const res = await this.ctx.service.backImage.setCurrent({
-        key: this.ctx.request.body.key,
-        user: this.ctx.state.user.op,
-      });
-      this.ctx.success(res);
-    }
-    async getCurrent() {
-      const res = await this.ctx.service.backImage.getCurrent({
-        user: this.ctx.state.user.op,
-      });
-      this.ctx.success(res);
-    }
+module.exports = class BackImageController {
+  async setCurrent() {
+    const res = await this.ctx.service.backImage.setCurrent({
+      key: this.ctx.request.body.key,
+      user: this.ctx.state.user.op,
+    });
+    this.ctx.success(res);
   }
-
-  return BackImageController;
+  async getCurrent() {
+    const res = await this.ctx.service.backImage.getCurrent({
+      user: this.ctx.state.user.op,
+    });
+    this.ctx.success(res);
+  }
 };
