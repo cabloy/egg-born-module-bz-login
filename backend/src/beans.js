aee578ddb4415fdc3d1732b6f72609ce0ef4610c
zhennann <zhen.nann@icloud.com>
2023-12-15 19:59:30 +0800

    refactor: beans

--- backend/src/beans.js
+++ backend/src/beans.js
@@ -1,17 +1,14 @@
 const versionManager = require('./bean/version.manager.js');
 const atomLoginBackImage = require('./bean/atom.loginBackImage.js');
 
-module.exports = app => {
-  const beans = {
-    // version
-    'version.manager': {
-      mode: 'app',
-      bean: versionManager,
-    },
-    // atom
-    'atom.loginBackImage': {
-      bean: atomLoginBackImage,
-    },
-  };
-  return beans;
+module.exports = {
+  // version
+  'version.manager': {
+    mode: 'app',
+    bean: versionManager,
+  },
+  // atom
+  'atom.loginBackImage': {
+    bean: atomLoginBackImage,
+  },
 };
