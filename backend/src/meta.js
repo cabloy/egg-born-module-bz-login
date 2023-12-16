68edc0f0daca49b5e051745a1a560b3c00dc1b0c
zhennann <zhen.nann@icloud.com>
2023-12-16 13:52:55 +0800

    refactor: meta

--- backend/src/meta.js
+++ backend/src/meta.js
@@ -1,66 +1,65 @@
 const moduleInfo = module.info;
-module.exports = app => {
-  const schemas = require('./meta/validation/schemas.js');
-  const staticLayouts = require('./meta/static/layouts.js');
-  const staticResources = require('./meta/static/resources.js');
-  // meta
-  const meta = {
-    base: {
-      atoms: {
-        loginBackImage: {
-          info: {
-            bean: 'loginBackImage',
-            title: 'LoginBackImage',
-            tableName: 'bzLoginBackImage',
-            language: false,
-            category: false,
-            tag: false,
-            comment: false,
-            attachment: false,
-            layout: {
-              config: {
-                atomList: 'layoutAtomListLoginBackImage',
-              },
+
+const schemas = require('./meta/validation/schemas.js');
+const staticLayouts = require('./meta/static/layouts.js');
+const staticResources = require('./meta/static/resources.js');
+// meta
+const meta = {
+  base: {
+    atoms: {
+      loginBackImage: {
+        info: {
+          bean: 'loginBackImage',
+          title: 'LoginBackImage',
+          tableName: 'bzLoginBackImage',
+          language: false,
+          category: false,
+          tag: false,
+          comment: false,
+          attachment: false,
+          layout: {
+            config: {
+              atomList: 'layoutAtomListLoginBackImage',
             },
           },
-          actions: {
-            setCurrent: {
-              code: 101,
-              title: 'SetCurrent',
-              actionModule: moduleInfo.relativeName,
-              actionComponent: 'action',
-              icon: { f7: '::radio-button-unchecked' },
-              enableOnOpened: null,
-              directShowOnList: true,
-              directShowOnItem: true,
-              stage: 'formal',
-            },
-          },
-          validator: 'loginBackImage',
-          search: {
-            validator: 'loginBackImageSearch',
+        },
+        actions: {
+          setCurrent: {
+            code: 101,
+            title: 'SetCurrent',
+            actionModule: moduleInfo.relativeName,
+            actionComponent: 'action',
+            icon: { f7: '::radio-button-unchecked' },
+            enableOnOpened: null,
+            directShowOnList: true,
+            directShowOnItem: true,
+            stage: 'formal',
           },
         },
-      },
-      statics: {
-        'a-baselayout.layout': {
-          items: staticLayouts,
-        },
-        'a-base.resource': {
-          items: staticResources,
+        validator: 'loginBackImage',
+        search: {
+          validator: 'loginBackImageSearch',
         },
       },
     },
-    validation: {
-      validators: {},
-      keywords: {},
-      schemas,
-    },
-    index: {
-      indexes: {
-        bzLoginBackImage: 'createdAt,updatedAt,atomId',
+    statics: {
+      'a-baselayout.layout': {
+        items: staticLayouts,
+      },
+      'a-base.resource': {
+        items: staticResources,
       },
     },
-  };
-  return meta;
+  },
+  validation: {
+    validators: {},
+    keywords: {},
+    schemas,
+  },
+  index: {
+    indexes: {
+      bzLoginBackImage: 'createdAt,updatedAt,atomId',
+    },
+  },
 };
+module.exports = meta;
