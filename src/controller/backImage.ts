3eb55d01013415fa92bf046b6c0b564a4692f27f
zhennann <zhen.nann@icloud.com>
2024-01-23 20:53:59 +0800

    feat: controller

--- src/controller/backImage.ts
+++ src/controller/backImage.ts
@@ -3,15 +3,18 @@ import { ScopeModuleBzLogin } from '../index.js';
 
 @Controller()
 export class ControllerBackImage extends BeanBase {
+  @Use()
+  scope: ScopeModuleBzLogin;
+
   async setCurrent() {
-    const res = await this.ctx.service.backImage.setCurrent({
+    const res = await this.scope.local.backImage.setCurrent({
       key: this.ctx.request.body.key,
       user: this.ctx.state.user.op,
     });
     this.ctx.success(res);
   }
   async getCurrent() {
-    const res = await this.ctx.service.backImage.getCurrent({
+    const res = await this.scope.local.backImage.getCurrent({
       user: this.ctx.state.user.op,
     });
     this.ctx.success(res);
