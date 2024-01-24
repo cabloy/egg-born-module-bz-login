62f9ddc1c3bf71a31abff5c7d74569a0cff545e1
zhennann <zhen.nann@icloud.com>
2024-01-24 10:33:13 +0800

    feat: config locales

--- src/config/locale/en-us.ts
+++ src/config/locale/en-us.ts
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
   LoginBackImages: 'Login Back Images',
   SetCurrent: 'Set Current',
 };
--- src/config/locale/zh-cn.ts
+++ src/config/locale/zh-cn.ts
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
   LoginBackImage: '登录背景图',
   LoginBackImages: '登录背景图',
   SetCurrent: '设为当前',
