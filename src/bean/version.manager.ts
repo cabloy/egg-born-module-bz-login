daee1eb4d81c389d7dbc0df2e8b2c2cfc96ce1de
zhennann <zhen.nann@icloud.com>
2024-01-25 17:47:46 +0800

    feat: version

--- src/bean/version.manager.ts
+++ src/bean/version.manager.ts
@@ -1,4 +1,7 @@
-module.exports = class Version {
+import { Bean, BeanBase } from '@cabloy/core';
+
+@Bean({ scene: 'version' })
+export class Version extends BeanBase {
   async update(options) {
     if (options.version === 1) {
       // create table: bzLoginBackImage
@@ -42,4 +45,4 @@ module.exports = class Version {
   }
 
   async test() {}
-};
+}
