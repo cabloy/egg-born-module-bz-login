de41acffeb90c57fe0bd15aa8d32d42f58dac7d9
zhennann <zhen.nann@icloud.com>
2024-01-28 20:39:53 +0800

    feat: scopemodule

--- src/controller/backImage.ts
+++ src/controller/backImage.ts
@@ -1,5 +1,4 @@
-import { BeanBase, Controller, Use } from '@cabloy/core';
-import { ScopeModule } from '../resource/this.js';
+import { BeanBase, Controller } from '@cabloy/core';
 import { ScopeModule } from '../resource/this.js';
 
 @Controller()
